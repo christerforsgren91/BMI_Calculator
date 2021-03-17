9019a521e4a0786bc5af9ec011b8dd3c6da33318
Christer <christerforsgren91@gmail.com>
2021-03-17 14:48:53 +0100

    added 2 js files and created placeholder and button, run a Cypress test.

--- cypress/integration/userCanCheckTheBmi.feature.js
+++ cypress/integration/userCanCheckTheBmi.feature.js
@@ -2,5 +2,8 @@ describe('User can check their Bmi', () => {
     it('user can enter weight and height', () =>{
         cy.visit('http://localhost:3001')
         cy.contains('BMI Calculator')
+        cy.get('#weight').type('80')
+        cy.get('#height').type('188')
+        cy.get('#Calculate').click()
     })
 })
