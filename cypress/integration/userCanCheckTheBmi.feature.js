760c949a5b18c6d149d4b1b52ddc790f29fd6107
Christer <christerforsgren91@gmail.com>
2021-03-18 14:00:10 +0100

    built a BMI calculator, tested with cypress and some styling with css

--- cypress/integration/userCanCheckTheBmi.feature.js
+++ cypress/integration/userCanCheckTheBmi.feature.js
@@ -1,12 +1,14 @@
 describe('User can check their Bmi', () => {
     it('user can enter weight and height', () =>{
         cy.visit('http://localhost:3001')
-        cy.contains('BMI Calculator')
-        cy.get('#weight').type('80')
-        cy.get('#height').type('188')
-        cy.get('#Calculate').click()
+        cy.get('#weight').type('90')
+        cy.get('#height').type('186')
+        cy.get('#calculate').click()
     })
     it('displays a BMI value of 26.01', () => {
-        cy.get('#results').should('contain', '26.01')
+		cy.get('#results').should('contain', '26.01')
     })
+    it('displays a message of you are Overweight', () => {
+		cy.get('#results').should('contain', 'Overweight')
+	})
 })
